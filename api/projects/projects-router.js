ae6a0c11247d2878551bff35eff12cf7f5c8e3bf
naushinhoque <fnmh@yahoo.com>
2023-11-30 11:30:29 -0600

    get, get, post done

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,61 @@
 // Write your "projects" router here!
+const projectsModel = require('../projects/projects-model')
+
+const router = express.Router()
+// [GET] /api/projects
+// Returns an array of projects as the body of the response.
+// If there are no projects it responds with an empty array.
+router.get('/', async (req, res) => {
+    const projects = await projectsModel.get()
+    res.json(projects)
+})
+
+//  [GET] /api/projects/:id
+// Returns a project with the given id as the body of the response.
+// If there is no project with the given id it responds with a status code 404.
+router.get('/:id', async (req, res) => {
+    const id = parsInt(req.params.id)
+    const project = await projectsModel.get(id)
+
+    if(!project) {
+        res.status(404).send('Project not found')
+        return
+    }
+    res.json(project)
+})
+
+//  [POST] /api/projects
+// Returns the newly created project as the body of the response.
+// If the request body is missing any of the required fields it responds with a status code 400.
+router.post('/', async (req, res) => {
+    const { name, description, completed } = req.body
+
+    if (!name || !description) {
+        res.status(400).send('Missing required fields')
+        return
+    }
+
+    const newProject = await projectsModel.insert({
+        name,
+        description,
+        completed,
+    })
+    res.json(newProject)
+})
+
+//  [PUT] /api/projects/:id
+// Returns the updated project as the body of the response.
+// If there is no project with the given id it responds with a status code 404.
+// If the request body is missing any of the required fields it responds with a status code 400.
+
+
+//  [DELETE] /api/projects/:id
+// Returns no response body.
+// If there is no project with the given id it responds with a status code 404.
+
+
+//  [GET] /api/projects/:id/actions
+// Returns an array of actions (could be empty) belonging to a project with the given id.
+// If there is no project with the given id it responds with a status code 404.
+// Inside api/actions/actions-router.js build endpoints for performing CRUD operations on actions:
+
